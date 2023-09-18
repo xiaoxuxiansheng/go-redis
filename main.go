f7b3d5826b5a2aff1c3ec010af55179cd6ccebdb
weixuxu <weixuxu@didiglobal.com>
2023-09-18 21:37:37 +0800

    init

--- main.go
+++ main.go
@@ -1,47 +1,20 @@
 package main
 
 import (
-	"bufio"
-	"context"
 	"fmt"
-	"io"
-	"net"
 
+	"github.com/xiaoxuxiansheng/go-redis/database"
+	"github.com/xiaoxuxiansheng/go-redis/pkg/protocol"
+	"github.com/xiaoxuxiansheng/go-redis/server"
 	"github.com/xiaoxuxiansheng/go-redis/tcp"
 )
 
-type myHandler struct {
-}
-
-func (m *myHandler) Handle(ctx context.Context, conn net.Conn) {
-	reader := bufio.NewReader(conn)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
-
-		msg, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("eof")
-			} else {
-				fmt.Print(err)
-			}
-			return
-		}
-
-		conn.Write([]byte(msg))
-	}
-}
-
-func (m *myHandler) Close() error {
-	return nil
-}
-
 func main() {
-	if err := tcp.ListAndServce("localhost:6399", &myHandler{}); err != nil {
+	parser := protocol.NewParser()
+	db := database.NewDatabase()
+	handler := server.NewHandler(parser, db)
+
+	if err := tcp.ListAndServce("localhost:6399", handler); err != nil {
 		fmt.Print(err)
 	}
 }
